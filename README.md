b4b2d26976978d6dfb44bbb0ec4fc4d469e7d99d
Binjuhor <binjuhor@gmail.com>
2017-12-01 11:41:27 +0700

    Initialize source code

---
+++ README.md
@@ -0,0 +1,3 @@
+Child theme change the buy button text and fix the buy tiket url.
+How to install?
+Just download zip file and install it on Apperance -> Add new themes and install this theme.
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Functions for Citysoul child theme
+ * @author Binjuhor - <binjuhor@gmail.com>
+ */
+
+function citysoul_child_theme_style() {
+	wp_enqueue_style( 'citysoul_child-childtheme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'citysoul_child_theme_style' , 10);
+
+/* Insert custom functions below */
